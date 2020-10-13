a717b177c9b6eede0f84c7b83dffc6af80b5249d
machamer <l.a.machamer@gmail.com>
2020-10-12 20:48:40 -0400

    initial commit

--- nfl_mov/nfl_mov_stack.py
+++ nfl_mov/nfl_mov_stack.py
@@ -1,4 +1,7 @@
-from aws_cdk import core
+from aws_cdk import (
+    aws_lambda as _lambda,
+    core
+)
 
 
 class NflMovStack(core.Stack):
@@ -6,4 +9,10 @@ class NflMovStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
+        mov_lambda = _lambda.Function(
+            self, 'CalcMov',
+            runtime=_lambda.Runtime.PYTHON_3_8,
+            code=_lambda.Code.asset('lambda/calculate_mov'),
+            handler='calculate_mov.handler',
+            timeout=core.Duration.seconds(6)
+        )   
